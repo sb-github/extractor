f5bd323e6cec153ee7a580382faf94c1ebf19b95
Yevhenii R <jus1ace@gmail.com>
2018-01-05 13:46:56 +0200

    first commit

---
+++ src/test/java/com/crawler/extractor/ExtractorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crawler.extractor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExtractorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
