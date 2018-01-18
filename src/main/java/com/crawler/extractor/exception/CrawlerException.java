8fbfc3db5b89b12972c5f94e001220c5b5e61cb1
StasOmelchenko <omelchenko.stas.1997@gmail.com>
2018-01-18 16:23:12 +0200

    Added CrawlerException class

---
+++ src/main/java/com/crawler/extractor/exception/CrawlerException.java
@@ -0,0 +1,15 @@
+package com.crawler.extractor.exception;
+
+/**
+ * 
+ * @author Stas Omelchenko
+ * 
+ * @date 16 January 2018
+ *
+ */
+public class CrawlerException extends RuntimeException {
+	
+	public CrawlerException(String message) {
+		super(message);
+	}
+}
