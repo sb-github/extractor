830e3e5190fe2dcde1316276ccac6f80b0297ec7
Yevhenii <yevhenii.ryzhuk@gmail.com>
2018-01-22 15:35:17 +0200

    Done some minor changes

--- src/test/java/com/crawler/extractor/CrawlerConfServiceTest.java
+++ src/test/java/com/crawler/extractor/CrawlerConfServiceTest.java
@@ -1,104 +1,92 @@
 package com.crawler.extractor;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-
-import java.nio.charset.Charset;
 import java.util.Date;
-
 import org.bson.types.ObjectId;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
-
-import com.crawler.extractor.model.Crawler;
 import com.crawler.extractor.model.CrawlerConf;
-import com.crawler.extractor.model.Status;
 import com.crawler.extractor.repository.ICrawlerConfRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class CrawlerConfServiceTest {
 	
-	private final String URL_TO_UPDATE_CRAWLER_CONF = "extractor/rest/v1/crawler/conf";
+	private final String URL = "/extractor/rest/v1/crawler/conf";
+	private static int maxNumberActiveCrawlers = 4;
 	
-	@LocalServerPort
-	private int port;
-
-	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-
 	private MockMvc mockMvc;
-
+	private Date date = new Date();
 	private CrawlerConf crawlerConf;
-
+	
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 	
 	@Autowired
 	private ICrawlerConfRepository iCrawlerConfRepository;
-
-	private String mapToJson(Object object) throws JsonProcessingException {
-		ObjectMapper objMapper = new ObjectMapper();
-		return objMapper.writeValueAsString(object);
-	}
 	
+	@Autowired
+	private ObjectMapper objMapper;
+
 	@Before
 	public void setup() throws Exception {
 		mockMvc = webAppContextSetup(webApplicationContext).build();
 		crawlerConf = new CrawlerConf();
 		crawlerConf.setId(new ObjectId());
-		crawlerConf.setMaxNumberActiveCrawler(4);
-		crawlerConf.setCreatedDate(new Date());
-		crawlerConf.setCreatedDate(new Date());
+		crawlerConf.setMaxNumberActiveCrawler(maxNumberActiveCrawlers);
+		crawlerConf.setCreatedDate(date);
+		crawlerConf.setModifiedDate(date);
 		iCrawlerConfRepository.save(crawlerConf);
 	}
-	
+
 	@After
 	public void deleteCrawlerConf() throws Exception {
 		iCrawlerConfRepository.delete(crawlerConf.getId());
 	}
-	
-	
+
 	@Test
 	public void testUpdateCrawlerConf() throws Exception {
+		assertEquals(iCrawlerConfRepository.findOne(crawlerConf.getId()).getMaxNumberActiveCrawler(),
+				crawlerConf.getMaxNumberActiveCrawler());
 		crawlerConf.setMaxNumberActiveCrawler(2);
-		mockMvc.perform(
-				put(createURLWithPort(URL_TO_UPDATE_CRAWLER_CONF)).contentType(contentType).content(mapToJson(crawlerConf)))
-				.andExpect(status().isOk());
-		crawlerFieldComparison(iCrawlerConfRepository.findOne(crawlerConf.getId()));
+		mockMvc.perform(put(getURL()).contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(objMapper.writeValueAsBytes(crawlerConf))).andExpect(status().isOk());
+		assertEquals(iCrawlerConfRepository.findOne(crawlerConf.getId()).getMaxNumberActiveCrawler(),
+				crawlerConf.getMaxNumberActiveCrawler());
+		checkIfCrawlerConfIsNotNull(iCrawlerConfRepository.findOne(crawlerConf.getId()));
 	}
-	
+
 	@Test
 	public void testGetAllCrawlerConf() throws Exception {
-		mockMvc.perform(
-				get(createURLWithPort(URL_TO_UPDATE_CRAWLER_CONF))).andExpect(status().isOk());
-		assertThat(iCrawlerConfRepository.findAll()).as("Whether list of crawlers is empty").isNotEmpty();
-
-	}
-	
-	private String createURLWithPort(String uri) {
-		return "http://localhost:" + port + uri;
-	}
-	
-	private void crawlerFieldComparison(CrawlerConf returnedCrawlerConf) {
-		assertThat(returnedCrawlerConf).as("Whether object is not null").isNotNull();
-		assertThat(returnedCrawlerConf.getId()).as("Whether id field of object is not null").isNotNull();
-		assertThat(returnedCrawlerConf.getMaxNumberActiveCrawler()).as("Whether maxNumberActiveCrawler field of object is not null").isNotNull();
-		assertThat(returnedCrawlerConf.getCreatedDate()).as("Whether createdDate field of object is not null").isNotNull();
-		assertThat(returnedCrawlerConf.getModifiedDate()).as("Whether modifiedDate of object is empty").isNotNull();
+		mockMvc.perform(get(getURL())).andExpect(status().isOk());
+		assertThat(iCrawlerConfRepository.findAll()).isNotEmpty().as("Whether list of crawlers is empty");
+		assertEquals(iCrawlerConfRepository.findAll().size(), 1);
 	}
 
+	private void checkIfCrawlerConfIsNotNull(CrawlerConf crawlerConf) {
+		assertThat(crawlerConf).isNotNull().as("Whether object is not null");
+		assertThat(crawlerConf.getId()).isNotNull().as("Whether id field of object is not null");
+		assertThat(crawlerConf.getMaxNumberActiveCrawler()).isNotNull()
+				.as("Whether maxNumberActiveCrawler field of object is not null");
+		assertThat(crawlerConf.getCreatedDate()).isNotNull().as("Whether createdDate field of object is not null");
+		assertThat(crawlerConf.getModifiedDate()).isNotNull().as("Whether modifiedDate of object is empty");
+	}
+
+	private String getURL() {
+		return URL;
+	}
 }
