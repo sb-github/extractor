d047d415c3e2fdc80a57168f721e0fa0962a0edf
StasOmelchenko <omelchenko.stas.1997@gmail.com>
2018-01-22 13:59:43 +0200

    Added log4j to CrawlerController class

--- src/main/java/com/crawler/extractor/ExtractorApplication.java
+++ src/main/java/com/crawler/extractor/ExtractorApplication.java
@@ -5,18 +5,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.repository.support.MongoRepositoryFactory;
-import com.mongodb.MongoClient;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.data.mongodb.repository.support.MongoRepositoryFactory;
+import com.mongodb.MongoClient;
 
 
 @PropertySource(value = "classpath:application.properties")
+@PropertySource(value = "classpath:crawler.properties")
 @SpringBootApplication
 public class ExtractorApplication {
 
