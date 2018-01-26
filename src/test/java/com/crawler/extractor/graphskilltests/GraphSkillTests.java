2164fa1db76b64eb00184accea1fde8b2bd7576c
Yevhenii <yevhenii.ryzhuk@gmail.com>
2018-01-26 17:06:44 +0200

    Added tests for GraphSkill

--- src/test/java/com/crawler/extractor/graphskilltests/GraphSkillTests.java
+++ src/test/java/com/crawler/extractor/graphskilltests/GraphSkillTests.java
@@ -1,21 +1,12 @@
-package com.crawler.extractor;
+package com.crawler.extractor.graphskilltests;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -28,26 +19,21 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.crawler.extractor.model.Connect;
 import com.crawler.extractor.model.GraphSkill;
 import com.crawler.extractor.repository.IGraphSkillRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class IntegrationTest {
+public class GraphSkillTests {
 
 	private MockMvc mockMvc;
 
-	String URL = "extractor/crawler";
+	final String URL = "extractor/crawler";
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
