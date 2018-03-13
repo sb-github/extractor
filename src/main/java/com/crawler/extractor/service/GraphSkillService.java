022f9aae72725c1b4716c9393e2f53b1f65fcca3
Alexander <9torchuncky6@gmail.com>
2018-03-13 11:56:52 +0200

    hot fix
    add missing import

--- src/main/java/com/crawler/extractor/service/GraphSkillService.java
+++ src/main/java/com/crawler/extractor/service/GraphSkillService.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
